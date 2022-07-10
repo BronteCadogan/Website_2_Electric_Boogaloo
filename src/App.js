7ed5d6303b848bc2d715dc46ad6540a3c66f66d7
Bronte <brontecadogan@ufl.edu>
2022-07-10 11:57:24 -0400

    Added Basic Information

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import './App.css';
+import bronte from "./images/profile.png"
+import resume from "./files/BronteCadoganResume.pdf"
 
 // One Page Website 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Bront&euml; Cadogan</h1>
-        <h2>Bachelor of Science: Computer Engineering</h2>
+    <div className="App" id="homepage">
+      <header className="App-header" id="homepage">
+        <h1 id="name">Bront&euml; Cadogan</h1>
+        <h2 id="qualification">Bachelor of Science: Computer Engineering</h2>
       </header>
+      <img src={bronte} alt="Bronte Cadogan" id="profile"/>
+      <ul id="list">
+        <li>
+          <a href={resume} target="_blank"  rel="noreferrer">R&eacute;sum&eacute;</a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/brontecadogan/" target="_blank" rel="noreferrer">LinkedIn</a>
+        </li>
+        <li>
+          <a href="https://github.com/BronteCadogan" target="_blank" rel="noreferrer">GitHub</a>
+        </li>
+      </ul>
+
+      <section className="about">
+        
+        
+      </section>    
     </div>
   );
 }
